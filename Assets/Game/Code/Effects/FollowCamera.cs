b59a9abb709b879843cba809dda3591d6f8e2580
Ilias <dovara41@gmail.com>
2024-12-14 09:47:13 +0100

    Smooth camera.

---
+++ Assets/Game/Code/Effects/FollowCamera.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Assets.Game.Code.Effects
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform target;
+        [SerializeField]
+        [Range(0f, 1f)]
+        private float smoothTime;
+
+        private Vector3 offset = new Vector3(0, 0, -10);
+        private Vector3 currentVelocity = Vector3.zero;
+
+        private void LateUpdate()
+        {
+            Vector3 targetPosition = target.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
+        }
+    }
+}
