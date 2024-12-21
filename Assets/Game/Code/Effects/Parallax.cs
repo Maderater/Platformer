e8949804dd276bb3072465dbc9beee2122a73680
Ilias <dovara41@gmail.com>
2024-12-21 09:38:34 +0100

    Parallax.

---
+++ Assets/Game/Code/Effects/Parallax.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Assets.Game.Code.Effects
+{
+    public class Parallax : MonoBehaviour
+    {
+        [SerializeField]
+        [Range(0f, 1f)]
+        private float parallaxIntencity;
+
+        private Camera cam;
+        private float startPos;
+
+        private void Awake()
+        {
+            cam = Camera.main;
+
+            startPos = transform.localPosition.x;
+        }
+
+        private void Update()
+        {
+            Scroll();
+        }
+
+        private void Scroll()
+        {
+            float currentCameraPos = cam.transform.position.x;
+            float distance = currentCameraPos * (1 - parallaxIntencity);
+
+            transform.localPosition = new Vector3(startPos + distance, transform.localPosition.y, transform.localPosition.z);
+        }
+    }
+}
