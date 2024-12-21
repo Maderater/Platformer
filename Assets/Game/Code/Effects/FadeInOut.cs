f1cbbe3e21d3022fdb70c89bf32031e1861c1815
Ilias <dovara41@gmail.com>
2024-12-21 10:04:58 +0100

    Fade.

---
+++ Assets/Game/Code/Effects/FadeInOut.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Game.Code.Effects
+{
+    public class FadeInOut : MonoBehaviour
+    {
+        [SerializeField]
+        private CanvasGroup curtain;
+
+        private void Awake()
+        {
+            FadeOut();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                FadeIn(() =>
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                });
+            }
+        }
+
+        public void FadeIn(Action OnFadeIn)
+        {
+            StartCoroutine(FadeInCoroutine(OnFadeIn));
+        }
+
+        public void FadeOut()
+        {
+            StartCoroutine(FadeOffCoroutine());
+        }
+
+        private IEnumerator FadeInCoroutine(Action OnFadeIn)
+        {
+            curtain.alpha = 0;
+
+            while (curtain.alpha < 1)
+            {
+                yield return new WaitForSeconds(0.01f);
+
+                curtain.alpha += 0.02f;
+            }
+
+            OnFadeIn?.Invoke();
+        }
+
+        private IEnumerator FadeOffCoroutine()
+        {
+            curtain.alpha = 1;
+
+            while (curtain.alpha > 0)
+            {
+                yield return new WaitForSeconds(0.01f);
+
+                curtain.alpha -= 0.02f;
+            }
+        }
+    }
+}
