148e0d3e42439fd05bb8b81903c4f1b27ac88c25
Ilias <dovara41@gmail.com>
2025-01-25 10:09:19 +0100

    Theory 2.

---
+++ Assets/Game/Code/Theory2.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Theory2 : MonoBehaviour
+{
+    // Пустые переменные (до тех пор пока не будут проинициализированы)
+    private GameObject crystalGameObject;
+    private Transform crystalTransform;
+    private Rigidbody2D rb;
+    private BoxCollider2D col;
+
+    // Пустые переменные (до тех пор пока мы не дадим ссылку для них в инспекторе)
+    public Rigidbody2D publicRb;
+    [SerializeField]
+    private Rigidbody2D sereilizedRb;
+
+    private void Awake()
+    {
+        // Инициализация компонентов
+        crystalGameObject = gameObject;
+        crystalTransform = transform; // GetComponent<Transform>()
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    private void Start()
+    {
+        // Изменение позиции на старте
+        crystalTransform.position = Vector3.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Сравнение с тегом игрока для детекта ингрока вошедшего в триггер
+        if (collision.CompareTag("Player"))
+        {
+            // Удаление компонентов
+            Destroy(rb);
+            Destroy(col);
+
+            // Удаление объекта
+            Destroy(crystalGameObject);
+        }
+    }
+}
