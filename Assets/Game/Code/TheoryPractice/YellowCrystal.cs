b846f28da2065eac73242032a8bff6bdebbe9a1a
Maderator <Dayanachoo12@gmail.com>
2025-02-08 10:43:00 +0200

    RedCrystal

--- Assets/Game/Code/TheoryPractice/YellowCrystal.cs
+++ Assets/Game/Code/TheoryPractice/YellowCrystal.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using Assets.Game.Code.Character;
+using UnityEngine;
 
 namespace Assets.Game.Code.TheoryPractice
 {
+    
     public class YellowCrystal : MonoBehaviour
     {
         // Сделать желтый кристалл.
@@ -9,5 +11,39 @@ namespace Assets.Game.Code.TheoryPractice
         // Сделать детектор соприкасаний с кристалом (триггер) с проверкой что его коснулся игрок.
         // Удалять кристалл после соприкосновения с игроком.
         // Спавнить в указанных 4 точках по красному кристаллу.
+
+        [SerializeField]
+        private Transform groundPoint1;
+
+        [SerializeField]
+        private Transform groundPoint2;
+
+        [SerializeField]
+        private Transform groundPoint3;
+
+        [SerializeField]
+        private Transform groundPoint4;
+
+        [SerializeField]
+        private GameObject redCrystal;
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                Instantiate(redCrystal, groundPoint1.position, Quaternion.identity);
+                Instantiate(redCrystal, groundPoint2.position, Quaternion.identity);
+                Instantiate(redCrystal, groundPoint3.position, Quaternion.identity);
+                Instantiate(redCrystal, groundPoint4.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+
+            
+        }
+
+
     }
+        
+
 }
