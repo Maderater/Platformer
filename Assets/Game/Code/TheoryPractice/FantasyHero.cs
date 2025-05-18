40e477ae71fe407e61ac9bfdeeb373190fa1bbc5
Ilias <dovara41@gmail.com>
2025-05-18 21:04:50 +0200

    Practice 2

--- Assets/Game/Code/TheoryPractice/FantasyHero.cs
+++ Assets/Game/Code/TheoryPractice/FantasyHero.cs
@@ -1,27 +1,39 @@
 using UnityEngine;
 
-// ???????????? ????
+// Namespace
 namespace Assets.Game.Code.TheoryPractice
 {
-    // ?????
+    // class
     public class FantasyHero
     {
-        // ??????????
+        // variables
         private string name;
         private int hp;
         public int damage;
         private float armor;
-        
-        // ???????????
+        private int num = 45;
+
+        // properties
+        public int Number1 { get; set; } = 15;
+        public int Number2 { get; private set; } = 25;
+        public int Number3 { get; } = 35;
+        public int Number4 { 
+            get { return num; }
+            set { num = value; }
+        }
+
+        // constructor
         public FantasyHero(string name, int hp, int damage, float armor)
         {
             this.name = name;
             this.hp = hp;
             this.damage = damage;
             this.armor = armor;
+
+            Number3 = 10;
         }
         
-        // ?????
+        // method
         public void TakeDamage(int damage)
         {
             float finalDamage = damage * (1f - armor);
@@ -29,10 +41,13 @@ namespace Assets.Game.Code.TheoryPractice
             hp -= (int)finalDamage;
         }
 
-        // ?????
+        // method
         public void ShowInfo()
         {
             Debug.Log($"{name}. HP: {hp}. Damage: {damage}. Armor: {armor}");
+
+            //Number2 = 1; // OK
+            //Number3 = 1; // Error
         }
     }
 }
--- Assets/Game/Code/TheoryPractice/Practice.cs
+++ Assets/Game/Code/TheoryPractice/Practice.cs
@@ -8,6 +8,11 @@ namespace Assets.Game.Code.TheoryPractice
         private FantasyHero warden;
         private FantasyHero nekromant;
 
+        private float hp = 10.512842411221415521312414212f;
+        private decimal money = 10.512842411221415521312414212m;
+
+        //private int parseResult = 10;
+
         private void Awake()
         {
             worrior = new FantasyHero("Worrior", 100, 40, 0.3f);
@@ -31,6 +36,42 @@ namespace Assets.Game.Code.TheoryPractice
                 warden.TakeDamage(worrior.damage);
                 nekromant.TakeDamage(warden.damage);
                 worrior.TakeDamage(nekromant.damage);
+            }
+
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                bool result = int.TryParse("350a", out int parseResult);
+                print("result: " + result);
+
+                //int parseResult = int.Parse("350a");
+                //print(parseResult);
+
+                //string text = "Mark25";
+                //print(text);
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                print("F: " + hp);
+                print("D: " + money);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                print("Number1: " + worrior.Number1); // 15
+                print("Number2: " + worrior.Number2); // 25
+                print("Number3: " + worrior.Number3); // 10
+                print("Number4: " + worrior.Number4); // 45
+
+                worrior.Number1 = 1;
+                //worrior.Number2 = 1; // Error
+                //worrior.Number3 = 1; // Error
+                worrior.Number4 = 1;
+
+                print("Number1: " + worrior.Number1); // 1
+                print("Number2: " + worrior.Number2); // 25
+                print("Number3: " + worrior.Number3); // 10
+                print("Number4: " + worrior.Number4); // 1
             }
         }
     }
